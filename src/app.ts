5debea1fb97fe46df1ee6b938dde61430e83892a
Leandro Rocha <leandro.wrocha98@gmail.com>
2024-05-27 21:33:33 -0300

    aula: integração com prisma orm

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
 
 // ORM - Object Relational Mapper
