f6d32891c758f834b6fd285b355c096a74800bd1
Leandro Rocha <leandro.wrocha98@gmail.com>
2024-06-04 22:22:48 -0300

    aula: caso de uso e design patterns

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,31 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({ name, email, password })
+  } catch (erro) {
+    if (erro instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: erro.message })
+    }
+
+    throw erro
+  }
+
+  return reply.status(201).send()
+}
